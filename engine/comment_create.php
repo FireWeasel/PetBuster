caba1e63efaf08a22519b125cc9ac82dcd70a5c3
Smolkar <dimo.popov97@gmail.com>
2017-03-29 01:05:34 +0200

    Simple eddits

--- engine/comment_create.php
+++ engine/comment_create.php
@@ -1,32 +1,7 @@
 <?php
   include "db_connect.php";
   session_start();
-if(isset($_FILES['file'])){
-    $file = $_FILES['file'];
 
-
-    $file_name = $file['name'];
-    $file_tmp = $file['tmp_name'];
-    $file_size = $file['size'];
-    $file_error = $file['error'];
-    $file_ext = explode('.', $file_name);
-    $file_ext = strtolower(end($file_ext));
-    $allowed = array('png', 'jpg', 'jepg');
-
-    if(in_array($file_ext, $allowed)){
-        if($file_error === 0){
-            $file_name_new = uniqid('', true) . '.' . $file_ext;
-            $file_destination = '../uploads/' . $file_name_new;
-            if(move_uploaded_file($file_tmp, $file_destination)){
-
-            }
-
-        }
-    }
-}
-else{
-  
-}
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $body = $_POST['comment-body'];
     $author = $_SESSION['username'];
